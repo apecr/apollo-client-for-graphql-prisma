a5dbde3665dce1a85681035bf855c127da057fae
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2020-03-24 12:55:09 +0100

    feat() First commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+/* global document*/
+import ApolloBoost, { gql } from 'apollo-boost'
+
+const client = new ApolloBoost({
+  uri: 'http://localhost:4000'
+})
+
+const getUsers = gql`
+    query{
+        users{
+            id
+            name
+        }
+    }
+`
+
+const getPosts = gql`
+    query{
+        posts{
+            id
+            title
+            author{
+                name
+            }
+        }
+    }
+
+`
+
+client.query({
+  query: getUsers
+}).then(response => {
+  let html = ''
+  response.data.users.forEach(user => {
+    html += `
+        <div>
+            <h3>${user.name}</h3>
+        </div>
+      `
+  })
+  document.getElementById('users').innerHTML = html
+})
+
+client.query({
+  query: getPosts
+}).then(response => {
+  document.getElementById('posts').innerHTML = response.data.posts.reduce((acc, post) => {
+    return acc += `
+        <div>
+            <h2>${post.title}</h2>
+            <h3>${post.author.name}</h3>
+        </div>
+      `
+  }, '')
+
+})
